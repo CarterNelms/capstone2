b142070db9c7cdf578d7c4169dcf253d62c2da8b
CarterNelms <carternelms@bellsouth.net>
2014-09-25 13:54:45 -0500

    cleaned up google places javascript

--- app/assets/javascripts/google_maps_places_async.js
+++ app/assets/javascripts/google_maps_places_async.js
@@ -1,51 +1,51 @@
-var $input
+var $input;
 
 function loadMap() {
 
-  var input = $input[0]
-  var place = null
-  var autocomplete = new google.maps.places.Autocomplete(input)
+  var input = $input[0];
+  var place = null;
+  var autocomplete = new google.maps.places.Autocomplete(input);
 
   // Google Map variables
-  var map = null
-  var marker = null
+  var map = null;
+  var marker = null;
 
   function initMap(){
-    var myLatlng = new google.maps.LatLng(36.1667,-86.7833)
-    var mapOptions = {zoom: 2, center: myLatlng}
+    var myLatlng = new google.maps.LatLng(36.1667,-86.7833);
+    var mapOptions = {zoom: 2, center: myLatlng};
 
-    map = new google.maps.Map($map[0], mapOptions)
+    map = new google.maps.Map($map[0], mapOptions);
 
     function updateMap(coordinates=null){
       // place = if targetPlace then autocomplete.getPlace(targetPlace) else autocomplete.getPlace()
       if(!coordinates){
-        place = autocomplete.getPlace()
+        place = autocomplete.getPlace();
         coordinates = {
           latitude: place.geometry.location.lat(),
           longitude: place.geometry.location.lng()
-        }
+        };
       }
-      var newlatlong = new google.maps.LatLng(coordinates.latitude, coordinates.longitude)
-      if(!marker){marker = new google.maps.Marker({ map: map })}
-      marker.setTitle($input.val())
-      map.setCenter(newlatlong)
-      marker.setPosition(newlatlong)
-      map.setZoom(12)
+      var newlatlong = new google.maps.LatLng(coordinates.latitude, coordinates.longitude);
+      if(!marker){marker = new google.maps.Marker({ map: map });}
+      marker.setTitle($input.val());
+      map.setCenter(newlatlong);
+      marker.setPosition(newlatlong);
+      map.setZoom(12);
     }
 
-    if(typeof(coords) != 'undefined') updateMap(coords)
+    if(typeof(coords) != 'undefined'){ updateMap(coords); }
 
     // Add listener to detect autocomplete selection
-    google.maps.event.addListener(autocomplete, 'place_changed', updateMap)
+    google.maps.event.addListener(autocomplete, 'place_changed', updateMap);
   }
 
-  initMap()
+  initMap();
 }
 
 $(function(){
 
-  $input = $('input#user_location')
-  $map = $('#map-canvas')
+  $input = $('input#user_location');
+  $map = $('#map-canvas');
 
   // The location field should be empty initially
   // $input.val('')
@@ -58,9 +58,9 @@ $(function(){
   }
 
   if(typeof(google) != 'undefined' && $map.length){
-    shouldLoadScript = typeof(google.maps) == 'undefined'
-    nextStep = shouldLoadScript ? loadScript : loadMap
-    $(document).ready(nextStep)
+    shouldLoadScript = typeof(google.maps) == 'undefined';
+    nextStep = shouldLoadScript ? loadScript : loadMap;
+    $(document).ready(nextStep);
   }
   // $(document).ready(loadScript)
 });
